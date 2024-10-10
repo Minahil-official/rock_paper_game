26d10ca14a3d63669d17c88cf6bdf0873685262d
Minahil Nawaz <minahilnawaz98@gmail.com>
2024-10-10 14:26:12 +0500

    other method

--- paper_game.py
+++ paper_game.py
@@ -1,15 +1,25 @@
+
 import random
-comp_num = random.randint(0,2)
-your_num = int(input("enter your number(0 for rock, 1 for paper,2 for scissors )\n"))
-print(f"your number is:",your_num)
-print(f"computer's number is:",comp_num)
-choice = {0:'rock',1:'paper',2:'scissors'}
+
+comp_num = random.randint(0, 2)
+
+your_num = int(input("Enter your number (0 for rock, 1 for paper, 2 for scissors):\n"))
+print(f"Your number is: {your_num}")
+print(f"Computer's number is: {comp_num}")
+
+choice = {0: 'rock', 1: 'paper', 2: 'scissors'}
+
+print(f"You chose {choice[your_num]}, Computer chose {choice[comp_num]}.")
+
 if your_num == comp_num:
-    print("it's draw")
-elif (your_num == 0 and comp_num == 2) or(your_num == 1 and comp_num == 0)or (your_num == 2 and comp_num == 1):
-    print('you lose')
+    print("It's a draw!")
+elif (your_num == 0 and comp_num == 2) or (your_num == 1 and comp_num == 0) or (your_num == 2 and comp_num == 1):
+    print("You win!")
+elif (your_num == 0 and comp_num == 1) or (your_num == 1 and comp_num == 2) or (your_num == 2 and comp_num == 0):
+    print("You lose!")
 else:
-    print("invalid choice")
+    print("Invalid choice!")
+
 
 
 
---
+++ rock.py
@@ -0,0 +1,35 @@
+import random
+comp_num = random.randint(0,2)
+print(comp_num)
+
+if comp_num == 2:
+      print('scissors')
+num = int(input('enter your number\n'))
+print(f'your number is',num)
+if num == 0:
+    print('your choice is rock')
+
+if   comp_num == 0: 
+        print(" computer's choice is rock")
+if   num == 1:
+    print('your choice is paper')
+
+if comp_num == 1:
+    print("computer's choice is paper")
+
+if  num == 2:
+    print('your choice is scissors')
+
+if comp_num == 2:
+    print("computer's choice is scissors")
+if num == comp_num:
+    print("it's draw")
+elif (num == 0 and comp_num ==1)or (num == )
+if num >comp_num:
+    print('you won')
+if num < comp_num:
+    print('you lose')
+else:
+    ("invalid choice")
+    
+
